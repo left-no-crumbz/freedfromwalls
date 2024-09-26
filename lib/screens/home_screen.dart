33db8220270b8683f71a24af16da0842980d7307
AKSM-TFT <kylemariano1635@gmail.com>
2024-09-26 20:30:45 +0800

    fix: Custom 5-day per row Calendar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
-// import '../main.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,8 +10,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DateTime today = DateTime.now();
-  late String _currentDate;
-  late String _currentDay;
+  late String _currentMonth;
+  late String _currentYear;
+  DateTime firstDayOfMonth = DateTime(DateTime.now().year, DateTime.now().month, 1);
 
   @override
   void initState() {
@@ -23,19 +22,24 @@ class _HomePageState extends State<HomePage> {
 
   void _updateTime() {
     final now = DateTime.now();
-    final formattedDate = DateFormat('yMMMMd').format(now);
-    final formattedDay = DateFormat('EEEE').format(now);
+    final formattedMonth = DateFormat('MMMM').format(now);
+    final formattedYear = DateFormat('y').format(now);
 
     setState(() {
-      _currentDate = formattedDate;
-      _currentDay = formattedDay;
+      _currentMonth = formattedMonth;
+      _currentYear = formattedYear;
     });
   }
 
-  void _onSelectedDay(DateTime day, DateTime focusedDay) {
-    setState(() {
-      today = day;
-    });
+  List<DateTime> _generateDaysInMonth(DateTime date) {
+    List<DateTime> days = [];
+    DateTime firstDayOfMonth = DateTime(date.year, date.month, 1);
+
+    for (int i = 0; i < today.day; i++) {
+      days.add(firstDayOfMonth.add(Duration(days: i)));
+    }
+
+    return days;
   }
 
   @override
@@ -44,12 +48,22 @@ class _HomePageState extends State<HomePage> {
     final height = MediaQuery.of(context).size.height;
     final isSmallScreen = width < 450;
 
+    // Days of the current month
+    List<DateTime> daysInMonth = _generateDaysInMonth(today);
+
+    // Mood icons for specific dates
+    Map<DateTime, String> userMoods = {
+      DateTime(2024, 9, 20): 'lib/assets/images/emotions-angry.png',
+      DateTime(2024, 9, 15): 'lib/assets/images/emotions-love.png',
+      // Add more dates and mood images as needed
+    };
+
     return Material(
       color: const Color.fromRGBO(241, 243, 244, 1),
       child: Center(
         child: Column(
           children: [
-            //Intro--something
+            // Intro section
             Container(
               width: width,
               height: isSmallScreen ? height * 0.15 : height * 0.17,
@@ -84,67 +98,92 @@ class _HomePageState extends State<HomePage> {
                       IconButton(
                         onPressed: () {},
                         icon: Icon(Icons.settings),
-                      )
+                      ),
                     ],
                   ),
                 ],
               ),
             ),
 
-            //Calendar
-            Container(
-              width: width,
-              height: height * 0.6,
-              alignment: Alignment.center,
-              margin: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-              ),
-              child: TableCalendar(
-                rowHeight: height * 0.07,
-                focusedDay: today,
-                firstDay: DateTime(2000, 1, 1),
-                lastDay: DateTime.now(),
-                headerStyle: const HeaderStyle(
-                  titleCentered: true,
-                  formatButtonVisible: false,
-                  headerMargin: EdgeInsets.only(bottom: 10),
-                ),
-                calendarStyle: CalendarStyle(
-                  selectedDecoration: const BoxDecoration(
-                    color: Colors.black,
-                    shape: BoxShape.circle,
+
+            // Custom 5-Day per row Calendar Grid
+            Column(
+              children: [
+                Text(
+                  '$_currentMonth',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: height * 0.02
                   ),
-                  defaultDecoration: BoxDecoration(
-                      color: const Color(0xFFD6D6D6),
-                      shape: BoxShape.circle,
-                      border: Border.all(width: 1, color: Colors.black)),
-                  weekendDecoration: BoxDecoration(
-                      color: const Color(0xFFD6D6D6),
-                      shape: BoxShape.circle,
-                      border: Border.all(width: 1, color: Colors.black)),
-                  selectedTextStyle: const TextStyle(color: Colors.white),
-                  weekendTextStyle: const TextStyle(color: Colors.black),
-                  todayDecoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.3),
-                    shape: BoxShape.circle,
+                ),
+                Text(
+                  '$_currentYear',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: height * 0.015
                   ),
-                  todayTextStyle: const TextStyle(color: Colors.white),
                 ),
-                availableGestures: AvailableGestures.all,
-                selectedDayPredicate: (day) => isSameDay(day, today),
-                onDaySelected: _onSelectedDay,
-                daysOfWeekStyle: DaysOfWeekStyle(
-                  weekdayStyle: TextStyle(color: Colors.transparent),
-                  weekendStyle: TextStyle(
-                      color: Colors.transparent,
-                      backgroundColor: Color(0xff2d2d2d)),
+
+                SizedBox(
+                  height: height * 0.02,
                 ),
-              ),
+
+                Container(
+                  height: height * 0.5,
+                  margin: EdgeInsets.symmetric(horizontal: width * 0.05),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 5,
+                      childAspectRatio: 1.5,
+                      mainAxisSpacing: height * 0.015,
+                    ),
+                    itemCount: daysInMonth.length,
+                    itemBuilder: (context, index) {
+                      DateTime date = daysInMonth[index];
+
+                      // Normalize the date to remove the time part
+                      DateTime normalizedDate = DateTime(date.year, date.month, date.day);
+
+                      // Check if a mood exists for the current date
+                      bool hasMood = userMoods.containsKey(normalizedDate);
+
+                      return Container(
+                          margin: EdgeInsets.all(4.0),
+                          width: width * 0.12,
+                          height: width * 0.12,
+                          decoration: BoxDecoration(
+                            color: isSameDay(today, date) ? Colors.black : Colors.grey[300],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: hasMood ? Image.asset(
+                              userMoods[normalizedDate]!,
+                              width: width * 0.12,
+                              height: width * 0.12,
+                              fit: BoxFit.cover,
+                            )
+                                : Text(
+                                  '${date.day}',
+                                  style: TextStyle(
+                                    color: isSameDay(today, date) ? Colors.white : Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                          ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  // Helper function to check if two dates are the same
+  bool isSameDay(DateTime a, DateTime b) {
+    return a.year == b.year && a.month == b.month && a.day == b.day;
   }
 }
