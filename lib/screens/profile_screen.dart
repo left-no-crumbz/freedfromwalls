7ddf0fc882ed724d67b92e6c487957a9c6b18d2a
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2024-09-27 00:07:55 +0800

    feat(ui): Finish first profile page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -12,7 +12,7 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: [
           Row(
             children: [
@@ -31,7 +31,94 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
               const SizedBox(width: 16),
             ],
-          )
+          ),
+          Image.asset(
+            "lib/assets/images/avatars/avatar-1.png",
+            height: 200,
+            width: 200,
+          ),
+          const SizedBox(height: 16),
+          const NameBio(),
+          const SizedBox(height: 16),
+          const Favorite(keyStr: "Motto", value: "Favorite Motto"),
+          const Favorite(keyStr: "Food", value: "Favorite Food"),
+          const Favorite(keyStr: "Drink", value: "Favorite Drink"),
+          const Favorite(keyStr: "Film", value: "Favorite Film"),
+          const Favorite(keyStr: "Show", value: "Favorite Show"),
+          const Favorite(keyStr: "Song", value: "Favorite Song"),
+          const Favorite(keyStr: "Game", value: "Favorite Game"),
+          const Favorite(keyStr: "Book", value: "Favorite Book"),
+          const Favorite(keyStr: "Color", value: "Favorite Color"),
+          const Divider(color: Colors.grey),
+          const SizedBox(height: 32)
+        ],
+      ),
+    );
+  }
+}
+
+class NameBio extends StatefulWidget {
+  const NameBio({super.key});
+
+  @override
+  State<NameBio> createState() => _NameBioState();
+}
+
+class _NameBioState extends State<NameBio> {
+  String? name;
+  String? bio;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          name ?? "Name",
+          style: const TextStyle(fontSize: 20),
+        ),
+        Text(
+          bio ?? "Bio",
+          style: const TextStyle(fontSize: 14),
+        )
+      ],
+    );
+  }
+}
+
+class Favorite extends StatefulWidget {
+  final String keyStr;
+  final String value;
+  const Favorite({super.key, required this.keyStr, required this.value});
+
+  @override
+  State<Favorite> createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          const Divider(color: Colors.grey),
+          Table(
+            columnWidths: const {
+              0: FixedColumnWidth(100),
+              1: FlexColumnWidth(),
+            },
+            children: [
+              TableRow(
+                children: [
+                  Text(
+                    widget.keyStr,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Color(0xff9C98CB)),
+                  ),
+                  Text(widget.value),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
