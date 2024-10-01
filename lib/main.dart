f439836510c85a538797f19a5e0e808f667420da
pistingbuhayt <patricksigua007@gmail.com>
2024-10-01 22:45:01 +0800

    feat : login_main {AwesomeNotification()}, build.gradle { compileSdkVersion 34          minSdkVersion 21  targetSdkVersion 34 }
     feel_screen.dart { added AwesomeNotification function }

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import './assets/widgets/custom_appbar.dart';
 import './assets/widgets/custom_bottom_navigation_bar.dart';
