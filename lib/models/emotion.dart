d6472df8e903d41b446c8d83394cb469a3678b31
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2024-10-04 14:56:21 +0800

    chore: Add data models

---
+++ lib/models/emotion.dart
@@ -0,0 +1,13 @@
+class Emotion {
+  int id;
+  String title;
+  String name;
+  String color;
+
+  Emotion({
+    required this.id,
+    required this.name,
+    required this.title,
+    required this.color,
+  });
+}
