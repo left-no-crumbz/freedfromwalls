9d0021244bade16e27c387127757e378a4ec2563
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2024-10-07 09:52:53 +0800

    chore: Create EmotionModel class

--- lib/models/emotion.dart
+++ lib/models/emotion.dart
@@ -1,13 +1,31 @@
-class Emotion {
-  int id;
+class EmotionModel {
+  int? id;
   String title;
   String name;
   String color;
 
-  Emotion({
-    required this.id,
+  EmotionModel({
+    this.id,
     required this.name,
     required this.title,
     required this.color,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'name': name,
+      'color': color,
+    };
+  }
+
+  factory EmotionModel.fromJson(Map<String, dynamic> json) {
+    return EmotionModel(
+      id: json['id'],
+      title: json['title'],
+      name: json['name'],
+      color: json['color'],
+    );
+  }
 }
