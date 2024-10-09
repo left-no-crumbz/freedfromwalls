a244296e536e4296da299011aaa9f9bc8e84b964
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2024-10-09 11:13:04 +0800

    feat: Create User model

---
+++ lib/models/user.dart
@@ -0,0 +1,23 @@
+class UserModel {
+  String? username;
+  String email;
+  String password;
+
+  UserModel({this.username, required this.email, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'username': username,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      username: json['username'],
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+}
