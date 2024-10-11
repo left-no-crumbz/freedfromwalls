80a2f141ef259a07903dfbe771fe2e993b8c2e03
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2024-10-11 11:08:06 +0800

    feat: Create UserProvider

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:freedfromwalls/models/user.dart';
+
+class UserProvider with ChangeNotifier {
+  UserModel? _user;
+
+  UserModel? get user => _user;
+
+  void setUser(UserModel user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  void clearUser() {
+    _user = null;
+    notifyListeners();
+  }
+}
