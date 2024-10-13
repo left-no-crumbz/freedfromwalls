26bd857245cab236e03390e777fb7009fc61d825
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2024-10-13 13:50:11 +0800

    feat: update implementation of user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,12 +1,15 @@
 class UserModel {
+  int? id;
   String? username;
   String email;
   String password;
 
-  UserModel({this.username, required this.email, required this.password});
+  UserModel(
+      {this.id, this.username, required this.email, required this.password});
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'username': username,
       'email': email,
       'password': password,
@@ -15,6 +18,7 @@ class UserModel {
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
+      id: json['id'],
       username: json['username'],
       email: json['email'],
       password: json['password'],
